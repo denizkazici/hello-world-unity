b6d8b612883a88f0c11276484947bf9ceff17bdb
deniz kazıcı <deniz.kazici@std.yeditepe.edu.tr>
2022-03-07 19:06:11 +0300

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class code : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world!");
+
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)){
+            yaz();
+        }
+    }
+    void yaz()
+    {
+        print("entere bastın");
+    }
+}
